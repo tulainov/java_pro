590ba14532aedc76b357e86fb5f2af1103202434
Danylo Tulainov <danylo.tulainov@voith.com>
2023-10-11 10:00:33 +0200

    hw6 is done

--- before-maven/src/main/java/com/tulainov/homeworks/homework06/HomeworkApp.java
+++ before-maven/src/main/java/com/tulainov/homeworks/homework06/HomeworkApp.java
@@ -1,33 +1,48 @@
 package com.tulainov.homeworks.homework06;
 
-import java.util.GregorianCalendar;
 import java.util.Scanner;
 
 public class HomeworkApp {
 
     public static void main(String[] args) {
 
-//        System.out.println(HomeworkApp.checkSumSign());
+        HomeworkApp.startHomework();
+    }
 
-//        System.out.println(HomeworkApp.isSumMoreThanTen());
+    public static void startHomework() {
 
         HomeworkApp.printThreeWords();
-
-        System.out.println("*".repeat(19));
-
-        System.out.println(HomeworkApp.isSumMoreThanTen());
-
         System.out.println("*".repeat(19));
 
         System.out.println(HomeworkApp.checkSumSign());
-
         System.out.println("*".repeat(19));
 
         System.out.println(HomeworkApp.printColor());
-
         System.out.println("*".repeat(19));
 
         System.out.println(HomeworkApp.compareNumbers());
+        System.out.println("*".repeat(19));
+
+        System.out.println(HomeworkApp.isSumMoreThanTen());
+        System.out.println("*".repeat(19));
+
+        System.out.println("Checking if number 21 is positive.");
+        System.out.println(HomeworkApp.isPositiveNumber(21));
+        System.out.println("*".repeat(19));
+
+        System.out.println("Checking if -12 is negative number");
+        System.out.println(HomeworkApp.isNegative(-12));
+        System.out.println("*".repeat(19));
+
+        System.out.println("Printing a line 'bla bla bla' 4 times");
+        HomeworkApp.printNTimes("Bla bla bla", 4);
+        System.out.println("*".repeat(19));
+
+        System.out.println(HomeworkApp.isYearLeap());
+        System.out.println("*".repeat(19));
+
+        System.out.println();
+        System.out.println("The homework 6 is done!");
     }
 
     public static void printThreeWords() {
@@ -105,6 +120,58 @@ public class HomeworkApp {
             return true;
         } else {
             System.out.println("No, sadly it's not there...");
+            return false;
+        }
+    }
+
+    public static String isPositiveNumber(int number) {
+
+        if (number < 0) {
+            return "The number is negative";
+        } else {
+            return "The number is positive or 0";
+        }
+    }
+
+    public static boolean isNegative(int number) {
+
+        return number < 0;
+    }
+
+    public static void printNTimes(String line, int timesToPrint) {
+
+
+        if (timesToPrint <= 0) {
+            System.out.println("Impossible to complete");
+        } else {
+            for (int i = 0; i < timesToPrint; i++) {
+                System.out.println(line);
+            }
+        }
+    }
+
+    public static boolean isYearLeap() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write a year and we'll check if this year is a leap year:");
+
+        int year = scanner.nextInt();
+
+        if (year % 400 == 0) {
+            System.out.println("This is a leap year!");
+            return true;
+        } else if (year % 100 == 0) {
+            System.out.println("This year is not a leap year...");
+            return false;
+        } else if (year % 4 != 0) {
+        System.out.println("This year is not a leap year...");
+        return false;
+        } else if (year % 4 == 0) {
+            System.out.println("This year is a leap year!");
+            return true;
+        } else {
+            System.out.println("Impossible to complete. Wrong year is given.");
             return false;
         }
     }
